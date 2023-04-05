e34925fecb2084c160dba54b1ce70a27ff46885c
杨淑琪 <94372211+xbysq@users.noreply.github.com>
2023-04-05 16:09:09 +0800

    Update color_label.py

--- color_label.py
+++ color_label.py
@@ -24,13 +24,14 @@ def color_map():
     cmap[8] = np.array([128,128,128])  #
     cmap[9] = np.array([130,87,87])  #
     # 二值变化图生成
-    # cmap[0] = np.array([0, 0, 0])  # 黑色
-    # cmap[1] = np.array([255,255,255])# 白色
-    # cmap[2] = np.array([255, 255, 255])  # 白色
-    # cmap[3] = np.array([255, 255, 255])  # 白色
-    # cmap[4] = np.array([255, 255, 255])  # 白色
-    # cmap[5] = np.array([255, 255, 255])  # 白色
-    # cmap[6] = np.array([255, 255, 255])  # 白色
+    # cmap[0] = np.array([255, 255, 255])
+    # cmap[1] = np.array([0, 0, 255])
+    # cmap[2] = np.array([128, 128, 128])
+    # cmap[3] = np.array([0, 128, 0])
+    # cmap[4] = np.array([0, 255, 0])
+    # cmap[5] = np.array([128, 0, 0])
+    # cmap[6] = np.array([255, 0, 0])
+
     return cmap
 
 
